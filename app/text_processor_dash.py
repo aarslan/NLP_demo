c6124d3558d977a83b6210001c1c2ee71146945f
aarslan <a.bilgin.a@gmail.com>
2023-05-04 19:47:12 -0400

    various enhancements; reseeding possible

--- app/text_processor_dash.py
+++ app/text_processor_dash.py
@@ -6,24 +6,13 @@ from dash.dependencies import Input, Output, State
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances
+import plotly.io as pio
 import plotly.graph_objs as go
 import plotly.colors as colors
 
 from model import read_data, load_bert_model, load_transformed_data, project_to_manifold
 
-layout = go.Layout(
-    scene=dict(
-        xaxis=dict(showticklabels=False),
-        yaxis=dict(showticklabels=False),
-        zaxis=dict(showticklabels=False),
-    ),
-    showlegend=False,
-)
-
-fig = go.Figure(data=[], layout=layout)
-fig.update_layout(height=800, width=800)
-
-app = dash.Dash(__name__ , external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container([
     dbc.Row([
@@ -35,8 +24,8 @@ app.layout = dbc.Container([
                 debounce=True,
                 n_submit=0,
                 style={'width': 'auto'}),
-                html.Div(id='output_first')
-            ], width='auto', style={'margin-left': '15px', 'margin-top': '50px', 'margin-right': '15px'})
+                html.Div(id='output_first')]
+            , width='auto', style={'margin-left': '15px', 'margin-top': '50px', 'margin-right': '15px'})
     ]),
     dbc.Row([
         dbc.Col(
@@ -52,11 +41,12 @@ app.layout = dbc.Container([
             html.Div([
                 html.H5('Answer:'),
                 html.Div(id='output')
-            ], style={'margin-left': '15px', 'height': '800px', 'width': '100%', 'overflowY': 'scroll'}),
-            width=2
+            ], style={'margin-left': '15px', 'margin-top': '10px', 'height': '800px', 'width': '100%',
+                      'overflowY': 'scroll'}),
+            width=4
         ),
         dbc.Col(
-            dcc.Graph(id='scatter-plot', figure=fig),
+            dcc.Graph(id='scatter-plot'),
             width=12
         )
     ], style={'display': 'flex', 'flex-wrap': 'nowrap'}),
@@ -69,7 +59,9 @@ app.layout = dbc.Container([
                 'pages 2013–2018', style={'fontSize': 10}),
         ])
     ]),
-    dcc.Store(id='past-questions', data=[])
+    dcc.Store(id='past-questions', data=[]),
+    dcc.Store(id='prompt', data='placeholder'),
+    dcc.Store(id='figure-store')
 ], fluid=True
 )
 
@@ -88,8 +80,8 @@ data['Text_vec'] = Text_vec_raw
 embeddings = project_to_manifold(data)
 data['x'], data['y'], data['z'] = embeddings[:, 0], embeddings[:, 1], embeddings[:, 2]
 
-clusterer = KMeans(n_clusters=24, random_state=0, n_init="auto").fit(data[['x', 'y', 'z']])
-data['cluster'] = clusterer.labels_
+cluster_estimator = KMeans(n_clusters=24, random_state=0, n_init="auto").fit(data[['x', 'y', 'z']])
+data['cluster'] = cluster_estimator.labels_
 categories = data['cluster'].unique()
 # COLOR MAPPING
 # create a list of colors for each point in the trace. %24 is the length of colormap
@@ -105,36 +97,47 @@ def cb_render(n_submit, input_value):
     if n_submit > 0:
         return f"You entered: {input_value}"
 
+
+@app.callback(Output('prompt', 'data'),
+              [Input('seed-input', 'value')],
+              [Input('my-dropdown', 'value'),
+               State('past-questions', 'data')])
+def update_output(text_value, dropdown_value, past_q):
+    ctx = dash.callback_context
+    if ctx.triggered[0]['prop_id'] == 'seed-input.value':
+        print(f' prompt is "{text_value}"')
+        prompt = text_value
+        return prompt
+    elif ctx.triggered[0]['prop_id'] == 'my-dropdown.value':
+        print(f' prompt is "{dropdown_value}"')
+        past_q.append(dropdown_value)
+        prompt = dropdown_value
+        return prompt
+    else:
+        print(f' DASH NO UPDATE')
+        return dash.no_update
+
+
 @app.callback(
     Output('my-dropdown', 'options'),
-    [Input('seed-input', 'value'),
-     Input('my-dropdown', 'value')],
+    Input('prompt', 'data'),
     State('past-questions', 'data')
 )
-def update_options(val0, val1, past_q):
-    #######
-    # THIS IS WHERE THE CHOICES GET CREATED
+def update_options(prompt, past_q):
+    if prompt is not None:
+        vectors = model.encode([prompt])
+        distances = pairwise_distances(Text_vec, vectors, metric='cosine', n_jobs=4)
+        close_ix = np.argpartition(distances.T, 5)[0]
 
-    if not val1:  # of it vals[0] has changed
-        selected_value = val0
+        choices_raw = list(data.iloc[close_ix[:20]]['Text'].values)
+        choices = list(set(choices_raw).difference(set(past_q)))  # remove past questions
+        options = [{'label': f'Question: {i}', 'value': i} for i in choices]
+
+        # # Don't know what this one does
+        # if prompt is not None:
+        #     choices.append({'label': f'Question: {prompt}', 'value': prompt})
     else:
-        selected_value = val1
-        past_q.append(val1)
-
-    if not val0 and not val1:
-        selected_value = 'placeholder'
-
-    vecs = model.encode([selected_value])
-    distances = pairwise_distances(Text_vec, vecs, metric='cosine', n_jobs=4)
-    close_ix = np.argpartition(distances.T, 5)[0]
-
-    choices_raw = list(data.iloc[close_ix[:20]]['Text'].values)
-    choices = list(set(choices_raw).difference(set(past_q)))  # remove past questions
-    options = [{'label': f'Question: {i}', 'value': i} for i in choices]
-
-    # Don't know what this one does
-    if selected_value is not None:
-        choices.append({'label': f'Question: {selected_value}', 'value': selected_value})
+        options = []
     return options
 
 
@@ -156,9 +159,21 @@ def display_output(selected_value):
 @app.callback(
     dash.dependencies.Output('scatter-plot', 'figure'),
     [dash.dependencies.Input('my-dropdown', 'value')],
-    State('past-questions', 'data'))
-def update_scatter_plot(selected_category, past_q):
-
+    [State('past-questions', 'data'),
+     State('figure-store', 'data')])
+def update_scatter_plot(selected_category, past_q, fig_data):
+    layout = go.Layout(
+        scene=dict(
+            xaxis=dict(showticklabels=False),
+            yaxis=dict(showticklabels=False),
+            zaxis=dict(showticklabels=False),
+        ),
+        showlegend=False,
+        uirevision=1  # this is supposed to keep came same between updates. We use "True" here, but it can be any
+        # variable whose change should trigger an update. For us, there is none, so it's just True
+        # PS: it's not actually working
+    )
+    fig = pio.from_json(fig_data) if fig_data else go.Figure(layout=layout)
     to_show = data[data.Text.isin(past_q + [selected_category])]
     new_trace = go.Scatter3d(
         x=to_show["x"],
@@ -169,10 +184,21 @@ def update_scatter_plot(selected_category, past_q):
             # size=10,
             color=to_show['color']),
         hovertemplate='<b>%{text}</b><extra></extra>',
-        text=[title for title in to_show["Text"].values]  # [title for title in df.Title],
+        text=[title for title in to_show["Text"].values],
     )
+
     fig.add_trace(new_trace)
+    fig.update_layout(height=800, width=800)
+    # print(pio.to_json(fig.layout.scene.camera))
     return fig
+
+
+@app.callback(
+    Output('figure-store', 'data'),
+    [Input('scatter-plot', 'figure')])
+def update_store(fig):
+    fig_json = pio.to_json(fig)
+    return fig_json
 
 
 if __name__ == '__main__':
